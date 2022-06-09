67e62501824bc6e67fcf34bc412cfaa4d3ff904f
chrisd1100 <chrisd1100@users.noreply.github.com>
2022-06-09 14:14:40 -0400

    Linux dialogs using zenity

--- src/unix/linux/dialog.c
+++ src/unix/linux/dialog.c
@@ -4,11 +4,48 @@
 
 #include "matoya.h"
 
+#include <stdio.h>
+
+#include "tlocal.h"
+
 bool MTY_HasDialogs(void)
 {
-	return false;
+	return true;
 }
 
 void MTY_ShowMessageBox(const char *title, const char *fmt, ...)
 {
+	va_list args;
+	va_start(args, fmt);
+
+	char *msg = MTY_VsprintfD(fmt, args);
+
+	va_end(args);
+
+	// FIXME: Need to escape quotes
+	const char *cmd = MTY_SprintfDL("zenity --info --title=\"%s\" --text=\"%s\"", title, msg);
+
+	FILE *f = popen(cmd, "r");
+	if (f)
+		pclose(f);
+
+	MTY_Free(msg);
+}
+
+const char *MTY_OpenFile(const char *title, MTY_App *app, MTY_Window window)
+{
+	char *name = NULL;
+
+	FILE *f = popen("zenity --file-selection", "r");
+
+	if (f) {
+		name = mty_tlocal(MTY_PATH_MAX);
+
+		if (!fgets(name, MTY_PATH_MAX, f))
+			name = NULL;
+
+		pclose(f);
+	}
+
+	return name;
 }
