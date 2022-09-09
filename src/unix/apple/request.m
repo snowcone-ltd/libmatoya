0624dab8e9c8ed792e29910e38cfe41f8067bec3
chrisd1100 <chrisd1100@users.noreply.github.com>
2022-09-09 09:26:10 -0400

    Don't add port to URL if using a https w/ 443 or http w/ 80

--- src/unix/apple/request.m
+++ src/unix/apple/request.m
@@ -47,7 +47,11 @@ bool MTY_HttpRequest(const char *host, uint16_t port, bool secure, const char *m
 	const char *scheme = secure ? "https" : "http";
 	port = port > 0 ? port : secure ? 443 : 80;
 
-	const char *url = MTY_SprintfDL("%s://%s:%u%s", scheme, host, port, path);
+	bool std_port = (secure && port == 443) || (!secure && port == 80);
+
+	const char *url =  std_port ? MTY_SprintfDL("%s://%s%s", scheme, host, path) :
+		MTY_SprintfDL("%s://%s:%u%s", scheme, host, port, path);
+
 	[req setURL:[NSURL URLWithString:[NSString stringWithUTF8String:url]]];
 
 	// Request headers
