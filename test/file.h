b5af30ac074732a0591c8a7aafea44bbfd64e87f
chrisd1100 <chrisd1100@users.noreply.github.com>
2022-11-07 06:13:49 -0500

    Updated MTY_MoveFile testing

--- test/file.h
+++ test/file.h
@@ -72,9 +72,13 @@ static bool file_main (void)
 	test_cmp("MTY_DeleteFile1", !MTY_FileExists(full_path));
 	test_cmp("MTY_DeleteFile2", !MTY_DeleteFile(full_path)); // Make sure it handles missing file
 
-	MTY_MoveFile(full_path_2, full_path);
-	test_cmp("MTY_MoveFile1", !MTY_MoveFile(full_path_2, full_path));
+	test_cmp("MTY_MoveFile1", MTY_MoveFile(full_path_2, full_path));
 	test_cmp("MTY_MoveFile2", MTY_FileExists(full_path));
+	test_cmp("MTY_MoveFile3", !MTY_FileExists(full_path_2));
+
+	test_cmp("MTY_MoveFile4", !MTY_MoveFile(full_path_2, full_path));
+	test_cmp("MTY_MoveFile5", MTY_FileExists(full_path));
+	test_cmp("MTY_MoveFile6", !MTY_FileExists(full_path_2));
 
 	MTY_FileList *list = MTY_GetFileList(prefix, ".txt");
 	bool failed = true;
