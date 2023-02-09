e59466ed07800df74fc7034cb36d14e9325aeb77
chrisd1100 <chrisd1100@users.noreply.github.com>
2023-02-08 20:20:05 -0500

    Add _POSIX_C_SOURCE only where necessary

--- src/unix/system.c
+++ src/unix/system.c
@@ -4,6 +4,8 @@
 // If a copy of the MIT License was not distributed with this file,
 // You can obtain one at https://spdx.org/licenses/MIT.html.
 
+#define _POSIX_C_SOURCE 200112L // gethostname
+
 #include "matoya.h"
 
 #include <string.h>
