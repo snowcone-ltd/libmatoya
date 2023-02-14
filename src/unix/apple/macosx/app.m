aa5c094b892acf4956f5ab4516232b442ca24945
chrisd1100 <chrisd1100@users.noreply.github.com>
2023-02-14 14:11:42 -0500

    macOS pen improvements

--- src/unix/apple/macosx/app.m
+++ src/unix/apple/macosx/app.m
@@ -467,7 +467,7 @@ static Window *window_find_mouse(Window *me, NSPoint *p)
 
 // Pen
 
-static void window_pen_event(Window *window, NSEvent *event)
+static void window_pen_event(Window *window, NSEvent *event, bool pressed)
 {
 	NSPoint p = {0};
 	Window *cur = window_find_mouse(window, &p);
@@ -479,11 +479,11 @@ static void window_pen_event(Window *window, NSEvent *event)
 	evt.pen.pressure = (uint16_t) lrint(event.pressure * 1024.0f);
 	evt.pen.rotation = (uint16_t) lrint(event.rotation * 359.0f);
 	evt.pen.tiltX = (int8_t) lrint(event.tilt.x * 90.0f);
-	evt.pen.tiltY = (int8_t) lrint(event.tilt.y * 90.0f);
+	evt.pen.tiltY = (int8_t) lrint(event.tilt.y * -90.0f);
 	evt.pen.x = lrint(p.x * scale);
 	evt.pen.y = lrint(p.y * scale);
 
-	bool touching = event.buttonMask & NSEventButtonMaskPenTip;
+	bool touching = event.buttonMask & NSEventButtonMaskPenTip || pressed;
 
 	// INVERTED must be set while hovering, but ERASER should only be set by
 	// while TOUCHING is also true
@@ -541,8 +541,11 @@ static void window_mouse_button_event(Window *window, NSUInteger index, bool pre
 
 static void window_button_event(Window *window, NSEvent *event, NSUInteger index, bool pressed)
 {
-	if (window.app.pen_enabled && event.subtype == NSEventSubtypeTabletPoint) {
-		window_pen_event(window, event);
+	// An index of zero indicates a pen event
+	bool is_pen_press = event.buttonMask & NSEventButtonMaskPenTip || index == 0;
+
+	if (window.app.pen_enabled && event.subtype == NSEventSubtypeTabletPoint && is_pen_press) {
+		window_pen_event(window, event, pressed);
 
 	} else {
 		window_mouse_button_event(window, index, pressed);
@@ -633,7 +636,7 @@ static void window_motion_event(Window *window, NSEvent *event)
 	bool pen_in_range = event.subtype == NSEventSubtypeTabletPoint;
 
 	if (window.app.pen_enabled && pen_in_range) {
-		window_pen_event(window, event);
+		window_pen_event(window, event, false);
 
 	} else {
 		window_mouse_motion_event(window, event, pen_in_range);
